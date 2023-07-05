54124539056015771bd204c4869125b931b2a90c
samir <sc9018@ic.ac.uk>
2023-07-05 11:02:02 +0100

    outline

---
+++ loss.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+
+
+## loss function
+
+#generate a random time
+
+#make random tensor from gaussian
+
+#get marginal prob std
+
+#perturb data
+
+#use score net to get score
+
+
+#compute loss
