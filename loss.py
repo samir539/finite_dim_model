47da542a54047e13b805f93d9087ecb838189fdf
samir <sc9018@ic.ac.uk>
2023-07-06 09:47:21 +0100

    added loss function

--- loss.py
+++ loss.py
@@ -1,11 +1,12 @@
 import numpy as np
 import torch 
 import torch.nn as nn
-
+from score_net import scoreNet
+torch.manual_seed(0)
 
 #forward process
 ################
-def marginal_forward(time,sigma,std=False):
+def marginal_forward(time,sigma=torch.Tensor([25]),std=False):
     """
     We use this function to perturb the data to various time steps
     :param time: the time step to perturb to (numpy tensor)
@@ -13,7 +14,7 @@ def marginal_forward(time,sigma,std=False):
     :param std (optional): return the standard deviation 
     """
     marginal_forward_val = 1/(2*torch.log(sigma))*(sigma**(2*time)-1)
-    marginal_forward_val_std = torch.std(marginal_forward_val)
+    marginal_forward_val_std = torch.sqrt(marginal_forward_val)
     if std:
         return marginal_forward_val_std
     else:
@@ -29,7 +30,7 @@ def marginal_forward(time,sigma,std=False):
 ## loss function
 ################
 
-def loss_function(x, marginal_forward):
+def loss_function(x, marginal_forward,sigma=25):
     """
     Loss function to pass into 
     :param x: 
@@ -38,20 +39,19 @@ def loss_function(x, marginal_forward):
 
     #generate a random time
     random_time = torch.rand(1)
-
-
     #make random tensor from gaussian
     random_like = torch.randn_like(x)
-
-
-
     #get marginal prob std
-    
-    
-
+    marginal_prob_std = marginal_forward(random_time,std=True)
     #perturb data
-
+    x_perturbed = x + random_like*marginal_prob_std
     #use score net to get score
+    score_model = scoreNet()
+    score_estimate = score_model.forward(x_perturbed)
+    #compute loss
+    loss = (1/2)*torch.mean(torch.sum((score_estimate + (x_perturbed - x)/sigma**2)**2, dim=(1,2,3)))
+    return loss
 
 
-#compute loss
+img = torch.rand((1,1,28,28))
+print(loss_function(img,marginal_forward))
